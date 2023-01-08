70a03efa56e7950ce8fe0c99aded971bf2228a13
David S <99992909+david03130@users.noreply.github.com>
2023-01-08 21:33:47 +0100

    Cambios en database. Se parece a la otra app

--- lib/dao/todo_dao.dart
+++ lib/dao/todo_dao.dart
@@ -8,7 +8,14 @@ class TodoDao {
   //Adds new Todo records
   Future<int> createTodo(Todo todo) async {
     final db = await dbProvider.database;
-    var result = db.insert(todoTABLE, todo.toDatabaseJson());
+    // var result;
+    // if (db != null) {
+    //   result = db.insert(todoTABLE, todo.toDatabaseJson());
+    // } else {
+    //   result = null;
+    // }
+    var result = db!.insert(todoTABLE, todo.toDatabaseJson());
+
     return result;
   }
 
@@ -20,10 +27,10 @@ class TodoDao {
     List<Map<String, dynamic>> result;
     if (query != null) {
       if (query.isNotEmpty) {
-        result = await db.query(todoTABLE,
+        result = (await db?.query(todoTABLE,
             columns: columns,
             where: 'description LIKE ?',
-            whereArgs: ["%$query%"]);
+            whereArgs: ["%$query%"]))!;
       } else {
         result = List.empty();
       }
@@ -43,7 +50,7 @@ class TodoDao {
   Future<int> updateTodo(Todo todo) async {
     final db = await dbProvider.database;
 
-    var result = await db.update(todoTABLE, todo.toDatabaseJson(),
+    var result = await db!.update(todoTABLE, todo.toDatabaseJson(),
         where: "id = ?", whereArgs: [todo.id]);
 
     return result;
@@ -52,7 +59,7 @@ class TodoDao {
   //Delete Todo records
   Future<int> deleteTodo(int id) async {
     final db = await dbProvider.database;
-    var result = await db.delete(todoTABLE, where: 'id = ?', whereArgs: [id]);
+    var result = await db!.delete(todoTABLE, where: 'id = ?', whereArgs: [id]);
 
     return result;
   }
@@ -60,7 +67,7 @@ class TodoDao {
   //We are not going to use this in the demo
   Future deleteAllTodos() async {
     final db = await dbProvider.database;
-    var result = await db.delete(
+    var result = await db!.delete(
       todoTABLE,
     );
 
